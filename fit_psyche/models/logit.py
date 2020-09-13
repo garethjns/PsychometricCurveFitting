c2318e20b20922c6050228281b88d0c94ceef935
Gareth <garethgithub@gmail.com>
2020-09-13 20:21:17 +0100

    Add publish workflow

--- fit_psyche/models/logit.py
+++ fit_psyche/models/logit.py
@@ -1,5 +1,5 @@
 import numpy as np
 
 
-def logit(x: np.array, mean: float, var: float) -> np.array:
+def logit(x: np.array, mean: float, var: float) -> np.ndarray:
     return 1 / (1 + np.exp(-var * (x - mean)))
